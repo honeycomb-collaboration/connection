27a754e79e22fa1b26c8ecca34a243b52d68e45e
Naeemo <naeemo@qq.com>
2022-07-10 21:26:59 +0800

    feat add test golang server

---
+++ server/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+const MaxMessageSizeBytes = 2 * 1024 * 1024
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handler(responseWriter http.ResponseWriter, request *http.Request) {
+	log.Println("WebSocket new connection: " + request.Host)
+	if conn, err := upgrader.Upgrade(responseWriter, request, nil); err == nil {
+		// Set message size limit
+		conn.SetReadLimit(MaxMessageSizeBytes)
+
+		go func() {
+			log.Println("WebSocket Authenticate ok: ")
+			for {
+				messageType, rawBytes, err := conn.ReadMessage()
+				if err != nil {
+					log.Println("websocket read error:", err)
+					break
+				}
+				log.Println("websocket read message:", rawBytes)
+
+				// echo
+				_ = conn.WriteMessage(messageType, rawBytes)
+			}
+		}()
+	}
+}
+
+func Start(path string) {
+	http.HandleFunc(path, handler)
+	log.Println("WebSocket service running: " + path)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,1 @@
-function main() {
-    const ws = new WebSocket(
-        'wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self'
-    )
-    ws.onmessage = function (evt) {
-        console.debug(evt)
-    }
-    ws.onerror = function (error) {
-        console.error(error)
-    }
-    ws.onopen = function (evt) {
-        console.log(evt)
-        ws.send('hello')
-    }
-}
-
-main()
+export { Connection } from '@/connection'
