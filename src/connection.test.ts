202c3cfbec60f58cfadce9292be4001681ea6ac4
Naeemo <naeemo@qq.com>
2022-07-11 20:30:09 +0800

    fix connection test

--- src/connection.test.ts
+++ src/connection.test.ts
@@ -4,7 +4,7 @@ import { Connection, ConnectionState } from '@/connection'
 describe('Connection', function () {
     it('basic', async () =>
         new Promise<void>(function (resolve) {
-            const con = new Connection()
+            const con = new Connection('ws://localhost:9527/ws')
             expect(con).toBeInstanceOf(Connection)
             con.addEventListener('open', () => {
                 expect(con.state).not.toEqual(ConnectionState.CONNECTING)
