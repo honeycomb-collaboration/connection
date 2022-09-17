2d90663aaf678d177a3c318199ab6dd955d5c7dd
Naeemo <naeemo@qq.com>
2022-09-17 10:08:15 +0800

    chore update test ws server url

--- web/src/connection/connection.test.ts
+++ web/src/connection/connection.test.ts
@@ -4,9 +4,12 @@ import { Connection, ConnectionState } from '@/connection/connection'
 describe('Connection', function () {
     it('basic', async () =>
         new Promise<void>(function (resolve) {
-            const con = new Connection('ws://localhost:9898/ws', (message) => {
-                console.debug(message)
-            })
+            const con = new Connection(
+                'ws://localhost:9898/v1/ws',
+                (message) => {
+                    console.debug(message)
+                }
+            )
             expect(con).toBeInstanceOf(Connection)
             con.addEventListener('open', () => {
                 expect(con.state).not.toEqual(ConnectionState.CONNECTING)
