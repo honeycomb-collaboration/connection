fb539256666a378e737d3a130e365800a262847c
Naeemo <naeemo@qq.com>
2022-10-03 11:33:19 +0800

    feat sharedworker

--- web/src/sharedworker.ts
+++ web/src/sharedworker.ts
@@ -3,17 +3,24 @@ import { Connection } from '@/connection/connection'
 
 const logger = new Logger('SharedWorkerGlobal')
 
+// Url to MessagePort map, an url can be shared between multiple MessagePorts
 const urlToPorts = new Map<string, Set<MessagePort>>()
+// MessagePort to Connection map, a message port uses at most one connection, for now.
 const portToConn = new WeakMap<MessagePort, Connection>()
 
-function getConnection(url: string, port: MessagePort): Connection {
+/**
+ * Initialize the connection to an url if necessary
+ * @param url
+ * @param port
+ */
+function initConnection(url: string, port: MessagePort) {
     const preConn = portToConn.get(port)
     if (preConn) {
         return preConn
     }
     const ports = urlToPorts.get(url)
     const conn = new Connection(url, (message: string) =>
-        handleConnMessage(message, url)
+        handleServerMessage(message, url)
     )
     if (ports) {
         ports.add(port)
@@ -21,10 +28,14 @@ function getConnection(url: string, port: MessagePort): Connection {
         urlToPorts.set(url, new Set([port]))
     }
     portToConn.set(port, conn)
-    return conn
 }
 
-function onerror(port: MessagePort, messageEvent: MessageEvent) {
+/**
+ * Handle message error event on MessagePort
+ * @param port
+ * @param messageEvent
+ */
+function onPortMessageError(port: MessagePort, messageEvent: MessageEvent) {
     logger.error('port error', port, messageEvent)
     port.close()
     const conn = portToConn.get(port)
@@ -42,7 +53,12 @@ function onerror(port: MessagePort, messageEvent: MessageEvent) {
     })
 }
 
-function handleConnMessage(message: string, url: string) {
+/**
+ * Handle message from server
+ * @param message
+ * @param url
+ */
+function handleServerMessage(message: string, url: string) {
     logger.info('handle conn message', message, url)
     const ports = urlToPorts.get(url)
     ports?.forEach((port) => {
@@ -50,39 +66,32 @@ function handleConnMessage(message: string, url: string) {
     })
 }
 
+/**
+ * Send message to server
+ * @param port
+ * @param message
+ */
+function sendMessage(port: MessagePort, message: string | ArrayBufferLike) {
+    const conn = portToConn.get(port)
+    if (conn) {
+        conn.send(message)
+    } else {
+        logger.error('send before init', message)
+    }
+}
+
 onconnect = function (evt) {
     logger.debug('onconnect', evt)
     for (const port of evt.ports) {
-        port.onmessageerror = (evt) => onerror(port, evt)
+        port.onmessageerror = (evt) => onPortMessageError(port, evt)
         port.onmessage = function (messageEvent) {
             logger.debug('port message', port, messageEvent.data)
             if (messageEvent.data.t === 'init') {
                 const url = messageEvent.data.d
-                const ports = urlToPorts.get(url)
-                const conn = portToConn.get(ports?.values().next().value)
-                if (!conn) {
-                    const conn = new Connection(url, (message: string) =>
-                        handleConnMessage(message, url)
-                    )
-                    if (ports) {
-                        ports.add(port)
-                    } else {
-                        urlToPorts.set(url, new Set([port]))
-                    }
-                    portToConn.set(port, conn)
-                } else {
-                    ports?.add(port)
-                    portToConn.set(port, conn)
-                }
+                initConnection(url, port)
             } else {
-                const conn = portToConn.get(port)
-                if (conn) {
-                    conn.send(messageEvent.data.d)
-                } else {
-                    logger.error('send before init', messageEvent.data)
-                }
+                sendMessage(port, messageEvent.data.d)
             }
         }
     }
-    logger.debug(urlToPorts, portToConn)
 }
