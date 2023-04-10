c7c01cb0b0f12d877f4ec8d6577ca3047dec0366
Naeemo <naeemo@qq.com>
2023-04-10 21:06:22 +0800

    feat export types

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,9 @@
 import { workerWrap } from '@/sharedworker/wrap'
 import { Connection } from './connection/connection'
+import type {
+    IConnection,
+    ConnectionMessageHandler,
+} from './connection/connection'
 
 export { workerWrap, Connection }
+export type { IConnection, ConnectionMessageHandler }
